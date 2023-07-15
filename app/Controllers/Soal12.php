6b05a5fd5177f14bee4780e37ec7d8bbd900a6bf
Taqiy Asyam Listyawan <taqiyasyaml@gmail.com>
2023-07-15 09:07:01 +0700

    use curl library instead native

--- app/Controllers/Soal12.php
+++ app/Controllers/Soal12.php
@@ -2,21 +2,21 @@
 
 namespace App\Controllers;
 
+use Config\Services;
+
 class Soal12 extends BaseController
 {
     public function soal2($id)
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://dummyjson.com/products/' . urlencode($id));
-        $res = curl_exec($ch);
-        $res_http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
+        $client = Services::curlrequest();
+        $res = $client->get('https://dummyjson.com/products/' . urlencode($id));
+        $res_http_code = $res->getStatusCode();
         if ($res_http_code !== 200 && $res_http_code !== 404)
             return $this->response->setStatusCode(502);
         else {
             try {
-                $json_data = json_decode($res, true, 512, JSON_THROW_ON_ERROR);
-                return $this->response->setStatusCode($res_http_code)->setJSON($json_data);
+                $json_data = json_decode($res->getBody(), true, 512, JSON_THROW_ON_ERROR);
+                return $this->response->setStatusCode($res_http_code)->setJSON($json_data, false);
             } catch (\Throwable $th) {
                 return $this->response->setStatusCode(500);
             }
