c2d0aa25a7c91eaeca576a1c535d789a53fa8109
Taqiy Asyam Listyawan <taqiyasyaml@gmail.com>
2023-07-15 22:36:07 +0700

    fix regex soal 1

--- app/Controllers/Soal12.php
+++ app/Controllers/Soal12.php
@@ -10,8 +10,8 @@ class Soal12 extends BaseController
     {
         $limit = $this->request->getGet('limit');
         $skip = $this->request->getGet('skip');
-        $limit = is_int($limit) && $limit > 0 ? $limit : (is_string($limit) && preg_match("/$[1-9][0-9]*^/", $limit) ? intval($limit) : 30);
-        $skip = is_int($skip) && $skip >= 0 ? $skip : (is_string($skip) && preg_match("/$[0-9]+^/", $skip) ? intval($skip) : 0);
+        $limit = is_int($limit) && $limit > 0 ? $limit : (is_string($limit) && preg_match("/^[1-9][0-9]*$/", $limit) ? intval($limit) : 30);
+        $skip = is_int($skip) && $skip >= 0 ? $skip : (is_string($skip) && preg_match("/^[0-9]+$/", $skip) ? intval($skip) : 0);
         $client = Services::curlrequest();
         $res = $client->get('https://dummyjson.com/products?' . http_build_query(['limit' => $limit, 'skip' => $skip]));
         $res_http_code = $res->getStatusCode();
