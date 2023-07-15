2cbda007e35ef45320d8148fd53bfbc070037180
Taqiy Asyam Listyawan <taqiyasyaml@gmail.com>
2023-07-15 08:30:59 +0700

    add soal2 routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,8 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+$routes->get('/api/soal2/(:alphanum)','Soal12::soal2/$1');
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
